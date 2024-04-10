15654351b3b9f00ed0695e65223e2a73ccb9f995
tony0721 <antoinequesnel11@gmail.com>
2024-04-10 09:10:45 -0400

    Can switch between pages

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
 import dash
+import json
 import dash_core_components as dcc
 import dash_html_components as html
+from urllib.parse import parse_qs, urlparse
 from dash.dependencies import Input, Output, State
 from viz1 import show_viz1
 from viz2 import show_viz2
@@ -17,15 +19,7 @@ import plotly.graph_objects as go
 app = dash.Dash(__name__)
 app.title = 'Spotify Song Recommender'
 
-def showVizualizations(dict_pref):
-    return html.Div(children=[
-    dcc.Link('Go back to user preferences', href='/userpreferences'),
-    show_viz1(dict_pref),
-    show_viz2(dict_pref),
-    show_viz3(dict_pref),
-    getRecommendationsForDecade(2010, 2019, dict_pref)
-])
-    
+
 def show_user_preferences():
     return html.Div(children=[
         html.H1('User Preferences'),
@@ -34,9 +28,18 @@ def show_user_preferences():
             html.P(dict_pref['sous_genres']),
             html.H3('Artistes'),
             html.P(dict_pref['artistes']),
-            dcc.Link('Go to vizualizations', href='/viz')
+            dcc.Link('Go to vizualizations', href='/viz?dict_pref=' + json.dumps(dict_pref))
         ])
     ])
+
+def showVizualizations(dict_pref):
+    return html.Div(children=[
+    dcc.Link('Go back to user preferences', href='/userpreferences'),
+    show_viz1(dict_pref),
+    show_viz2(dict_pref),
+    show_viz3(dict_pref),
+    getRecommendationsForDecade(2010, 2019, dict_pref)
+])
 
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
@@ -44,15 +47,22 @@ app.layout = html.Div([
 ])
 
 @app.callback(Output('page-content', 'children'),
-              [Input('url', 'pathname')])
-def display_page(pathname):
-    if pathname == '/viz':
-        return showVizualizations(dict_pref)
-    elif pathname == '/userpreferences':
+              [Input('url', 'pathname'),
+               Input('url', 'href')])
+def display_page(pathname, href):
+    # Parse the URL to get the query parameters
+    parsed_url = urlparse(href)
+    query_params = parse_qs(parsed_url.query)
+
+    # Get the dict_pref query parameter
+    dict_pref_str = query_params.get('dict_pref', [None])[0]
+    if dict_pref_str is not None:
+        dict_pref = json.loads(dict_pref_str)
+    if pathname == '/userpreferences' or pathname == '/':
         return show_user_preferences()
+    elif pathname == '/viz':
+        return showVizualizations(dict_pref)
     else:
         return "404 Page Error! Please select a link"
-
-server = app.server
-
+    
 server = app.server
