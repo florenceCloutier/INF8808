da3d48f599979a072c63dc28ec5dc961d76c5f67
tony0721 <antoinequesnel11@gmail.com>
2024-04-15 17:12:36 -0400

    Better methodology

--- src/methodology.py
+++ src/methodology.py
@@ -1,16 +1,15 @@
 import dash_html_components as html
 
 methodology_text = """
-Méthodologie: Après avoir lu et préparé les données, les attributs numériques 
-ont été normalisé à l'aide de la mise à l'échelle Min-Max, les valeurs moyennes des 
-attributs ont été calculé pour chaque genre musical et chaque artiste, 
-ainsi que créé une carte associant les genres principaux à leurs sous-genres respectifs.
-Le profil de l'utilisateur correspond donc a une moyenne de tous les attributs des chansons
-contenu dans les artistes sélectionnés. Pour profiler les préférences des utilisateurs,
-une distance euclédienne a été utilisé entre les moyennes des attributs et le profil.
-Dans le cas d'une chanson individuelle la distance entre les attributs d'une chanson
-et le profil usager a été utilisé. Le jeux de données utilisés est disponible à 
-l'addresse suivante: https://www.kaggle.com/datasets/joebeachcapital/30000-spotify-songs
+Après avoir lu et préparé les données, les attributs numériques ont été normalisés 
+à l'aide de la mise à l'échelle Min-Max. Les valeurs moyennes de ces attributs ont été 
+calculées pour chaque genre musical et chaque artiste. De plus, une carte associant les
+genres principaux à leurs sous-genres respectifs a été créée. Le profil de l'utilisateur 
+correspond ainsi à une moyenne de tous les attributs des chansons contenues dans les sélections d'artistes. 
+Pour profiler les préférences des utilisateurs, une distance euclidienne a été utilisée 
+entre les moyennes des attributs et le profil de l'utilisateur. Dans le cas d'une chanson individuelle, 
+la distance entre les attributs de cette chanson et le profil de l'usager a également été calculée.
+Le jeu de données utilisé est disponible à l'adresse suivante :https://www.kaggle.com/datasets/joebeachcapital/30000-spotify-songs
 """
 
 def getMethodologyComponent():
