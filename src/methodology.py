4e5ed2db2aae635a9ef355c53944e9bd8c1c6da5
tony0721 <antoinequesnel11@gmail.com>
2024-04-15 19:15:27 -0400

    Moyenne methodology

--- src/methodology.py
+++ src/methodology.py
@@ -2,7 +2,7 @@ import dash_html_components as html
 
 methodology_text = """
 Après avoir lu et préparé les données, les attributs numériques ont été normalisés 
-à l'aide de la mise à l'échelle Min-Max. Les valeurs moyennes de ces attributs ont été 
+à l'aide de la mise à l'échelle par la moyenne. Les valeurs moyennes de ces attributs ont été 
 calculées pour chaque genre musical et chaque artiste. De plus, une carte associant les
 genres principaux à leurs sous-genres respectifs a été créée. Le profil de l'utilisateur 
 correspond ainsi à une moyenne de tous les attributs des chansons contenues dans les sélections d'artistes. 
